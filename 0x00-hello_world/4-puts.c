5dc25e05903c59e3aea8ae27b4ffe60de0190699
Salah <salahzeinu@gmail.com>
2022-06-30 00:50:12 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-# include<stdio.h>
+#include<stdio.h>
 /**
- * main - Entry Point 
+ * main	- Entry Point
  *
- * return: always (0) success
+ * Return: Always (0) (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	puts("/"Programming is like building is like multilingual puzzle")
+	return (0)
 }
