4829711d25fd30ca71c592f2ac1e842e4331a554
Salah <salahzeinu@gmail.com>
2022-06-30 00:40:19 -0700

    update final

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 # include<stdio.h>
 /**
- * main - Entry 
+ * main - Entry Point 
  *
  * return: always (0) success
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
-return (0);
+        return (0);
 }
