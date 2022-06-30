6db3ceb5517b4827c6569d73dde12d028ac1516b
Salah <salahzeinu@gmail.com>
2022-06-30 00:53:17 -0700

    adjust betys comment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	puts("/"Programming is like building is like multilingual puzzle")
-	return (0)
+	puts("/"Programming is like building is like multilingual puzzle");
+	return (0);
 }
