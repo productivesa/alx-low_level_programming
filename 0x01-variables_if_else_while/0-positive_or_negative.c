8616d55e195b454459ee98d5a7ca1280edb49629
Salah <salahzeinu@gmail.com>
2022-07-02 12:47:36 -0700

    disaply positive,negative or 0 based onn input

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <time.h>
+int main(void)
+{
+	int n;
+
+	strand(time(0));
+	n = rand() - RAND_MAX / 2;
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This is a C progrmming project that shows if -else,and loop statments
