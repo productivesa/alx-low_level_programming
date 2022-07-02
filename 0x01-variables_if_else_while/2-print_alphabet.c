b6abfcd5342d6ad0e2915295a01a2d7e574ac29c
Salah <salahzeinu@gmail.com>
2022-07-02 14:18:06 -0700

    smal letter alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - small alphabet display
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alphabet[i]);
+	}
+	putchar('\n');
+	return (0);
+}
