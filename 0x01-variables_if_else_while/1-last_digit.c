1680602d476c233fa5a098960753dc77a23d27db
Salah <salahzeinu@gmail.com>
2022-07-02 14:10:55 -0700

    last degit of n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,28 +2,28 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * Main - compairing 5, or 6, or 0
+ * main - display last degit of n
  *
  * Return: Always 0 (Success)
- * 
  */
 int main(void)
 {
-	int n, last;
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	last = n % 10;	
-	if (last < 5)
-	{
-		printf("The Last Degit Of %d is %d and is greater than 5\n", n, last);
-	}
-	else if(last == 0 )
-	{
-		priintf("the last degit of %d is %d and is 0\n", n, last);
-	}
-	else
-	{
-		printf("the last degit of %d is %d and is less than 6 and not 0\n", n, last);
-	}
-	return (0);
-}	
+		int n, last;
+
+		srand(time(0));
+		n = rand() - RAND_MAX / 2;
+		last = n % 10;
+		if (last > 5)
+		{
+		printf("Last digit of %d is %d and is greater than 5\n", n, last);
+		}
+		else if (last == 0)
+		{
+		printf("Last digit of %d is %d and is 0\n", n, last);
+		}
+		else
+		{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
+		}
+		return (0);
+}
