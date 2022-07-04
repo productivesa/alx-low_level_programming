0b92b1238d713ea87cc1f1ac441c5881532f3328
Salah <salahzeinu@gmail.com>
2022-07-04 04:36:00 -0700

    print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+char x = 0;
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(x)
+x++
+}
+x = '1'
+for (i = 0; i < 6; i++)
+x++;
+putchar('0' + x);
+putchar('\n');
+}
