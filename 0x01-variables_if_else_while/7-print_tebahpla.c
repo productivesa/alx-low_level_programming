2efebb3f0822af836a92eb24d687caaec63b1e28
Salah <salahzeinu@gmail.com>
2022-07-04 03:53:36 -0700

    print lower leeter reversily

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: Always return 0
+ */
+int main(void)
+{
+char num;
+for (num = 'z'; numb >= 'a'; numb--)
+putchar(numb)
+putchar('/n')
+return (0);
+}
