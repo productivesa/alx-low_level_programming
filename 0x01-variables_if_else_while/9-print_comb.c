3bc32c21d68c77b4e6eecb3028b7e0a43c0b78e5
Salah <salahzeinu@gmail.com>
2022-07-04 05:07:46 -0700

    print

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+int nu;
+for (nu = 0; nu <= 9; nu++)
+{
+putchar((nu % 10) + '0');
+if (nu == 9)
+putchar(',');
+putchar(' ');
+}
+
+putchar('/');
+return (0);
+}
