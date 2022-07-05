e8d0a1cd68bce2fc67620c6c421db70e994549cc
Salah <salahzeinu@gmail.com>
2022-07-05 16:58:21 -0700

    fib

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * function to prints valued even number
+ * Return: Always 0
+ */
+int main(void)
+{
+long int n1, n2, fn, fib;
+n1 = 1;
+n2 = 2;
+fn = fib = 0;
+while (fn <= 4000000)
+{
+fn = n1 + n2;
+n1 = n2;
+n2 = fn;
+if ((n1 % 2) == 0)
+{
+fib = fib + n1;
+}
+}
+printf("%d\n", fib);
+return (0);
+}
