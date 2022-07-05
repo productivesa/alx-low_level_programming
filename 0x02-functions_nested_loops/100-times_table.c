d55196b51ac7b5f11ae357c661e761e24d5396b7
Salah <salahzeinu@gmail.com>
2022-07-05 16:29:16 -0700

    times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * times-table - print times n times table
+ * Return: Always 0
+ */
+void print_times_table(int n)
+{
+int n, m, p;
+if (n >= 0 && n <= 15)
+{
+for (n = 0; n <= n; n++)
+{
+_putchar('0');
+for (m = 1; m <= n; m++)
+{
+_putchar(',');
+_putchar(' ');
+p = n * m;
+if (p <= 99)
+_putchar(' ');
+if (p <= 9)
+_putchar(' ');
+if (p >= 100)
+{
+_putchar((p / 100) + '0');
+_putchar(((p / 10)) % 10 + '0');
+}
+else if (p <= 99 && p >= 10)
+{
+_putchar((p / 10) + '0');
+}
+_putchar((p % 10) + '0');
+}
+_putchar('\n');
+}
+}
+}
