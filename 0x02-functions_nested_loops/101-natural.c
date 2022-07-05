c36a3d0e21caa07433e7e27af814b142d3b8aa73
Salah <salahzeinu@gmail.com>
2022-07-05 16:37:16 -0700

    natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,15 @@
+# include "main.h"
+/**
+ * main - Entry
+ * function to compute all multiples
+ * Return: Always 0
+ */
+int main(void)
+{
+int x;
+int sum = 0;
+for (x = 0; x < 1024; x++;
+sum = sum + x;
+}
+printf("%d\n", sum);
+return (0);
