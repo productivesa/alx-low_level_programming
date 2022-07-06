b39b42d0eb6ed7d4f2dd6b259446a9bf2078ed47
Salah <salahzeinu@gmail.com>
2022-07-05 17:41:49 -0700

    remove white sapce

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-
 /**
  * main - function Prints the first 98 Fibonacci numbers, starting with
- * 1 and 2, separated by a comma followed by a space.
- * 
+ * 1 and 2 separated by a comma followed by a space.
  * Return: Always 0
  */
 int main(void)
