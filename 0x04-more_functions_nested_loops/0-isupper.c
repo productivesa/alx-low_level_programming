0a192b6aa84ea2249906c0c4fba215617ed7f4e5
Salah <salahzeinu@gmail.com>
2022-07-07 14:42:52 -0700

    update

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,14 +5,14 @@
  * @c: input character
  * Return: 1 if is an uppercase character ,else 0
  */
-int _isupper(int z)
+int _isupper(int c)
 {
-	if (z >= 65 && z <= 90)
-	{
+	if (c >= 65 && c <= 90)
+		{
 		return (1);
-	}
+		}
 	else
-	{
-	return (0);
-	}
+		{
+		return (0);
+		}
 }
