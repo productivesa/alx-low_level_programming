482e6c44ed79ae876022c50aae1f338832112590
Salah <salahzeinu@gmail.com>
2022-07-11 14:24:14 -0700

    keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generate random valid passwords.
+ * Return: 0
+ */
+int main(void)
+{
+	int password;
+	int sum;
+
+	srand(time(null));
+	sum = 0;
+	while (sum <= 2645)
+	{
+		password = (rand() % 128);
+		sum = sum + pass;
+		printf("%c", password);
+	}
+	printf("%c", 2772 - sum);
+	return (0);
+}
+
