4190c0377373e42df7c53ba4f5654519216c9039
Salah <salahzeinu@gmail.com>
2022-07-18 06:08:24 -0700

    update

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -13,7 +13,7 @@ void print_diagsums(int *a, int size)
 	int n1 = 0;
 	int n2 = 0;
 
-	for (i = 0; i < (size * size); i++)
+	for (; i < (size * size); i++)
 	{
 		if (i % (size + 1) == 0)
 			n1 = n1 + *(a + i);
