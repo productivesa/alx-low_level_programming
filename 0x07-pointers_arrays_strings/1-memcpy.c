d946598bf08d678add632dd0f9dc27d8875d8802
Salah <salahzeinu@gmail.com>
2022-07-18 04:03:33 -0700

    update

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,7 +10,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int x;
 
-	for (x = 0 ; x < n; x++)
-		*(dest + i) = *(src + i);
+	for (x = 0; x < n; x++)
+		*(dest + x) = *(src + x);
 	return (dest);
 }
