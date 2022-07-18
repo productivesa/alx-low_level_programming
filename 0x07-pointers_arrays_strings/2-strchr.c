e23a0e5018fe4a2da718f4b4aaed9f2eb7cf29d4
Salah <salahzeinu@gmail.com>
2022-07-18 00:00:12 -0700

    memory copy

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _memcpy - copies memory area
+ * @des: memory area destination
+ * @src: constant byte
+ * @n: bytes filled
+ * Return: the pointer to dest
+ */
+char *_memcpy(char *dest, char *src,unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (i = 0; i < n; i++)
+		*(dest + i) = *(src + i);
+	return (dest);
+}
