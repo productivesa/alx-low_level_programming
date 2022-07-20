d0373dd25285d2b8eb71bccc4e55fcbaaba0b827
Salah <salahzeinu@gmail.com>
2022-07-20 06:16:12 -0700

    update

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -7,8 +7,11 @@
  */
 int wildcmp(char *str1, char *str2)
 {
-	if (str2 == * != '\0')	
-	if *(str2 + 1) != '\0') 
+	if (str2 == *)
+	if (str2 != '\0')
+		return (0);	
+	if *(str2 + 1) != '\0')
+	       	return (0);	
 	if *str1 == '\0')
 		return (0);
 	if (str1 == '\0' && *str2 == '\0')
