68a4d0d33eeeab719b614801049f7c217f10086a
Salah <salahzeinu@gmail.com>
2022-08-03 01:50:07 -0700

    advanced

---
+++ 0x0F-function_pointers/00-main_opcodes.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - 
+ * @argc: --
+ * @argv: --
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	char *opc = (char *) main;
+	int x, nbutes;
+
+	if (argc != 2)
+	{
+		printf("WError\n");
+		exit(1);
+	}
+	nbytes = atoi(argv[x]);
+	if (nbytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (x = 0; x < nbytes; x++)
+	{
+		printf("%02x", opc[x] & 0xFF);
+		if (x != nbytes - 1)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
