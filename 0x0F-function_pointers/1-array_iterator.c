894a1df0b1e1ee45c3b0573ad2eee07508950aae
Salah <salahzeinu@gmail.com>
2022-08-02 23:31:38 -0700

    exec fucnton

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_ppointers.h"
+/**
+ * array_iterator - execute a function
+ * @array: input
+ * @size: size of an array
+ * @action: pointer of the function
+ * Return: no return
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int x;
+
+	if (array && action)
+		for (x = 0; x < size; x++)
+			action(array[x]);
+}
