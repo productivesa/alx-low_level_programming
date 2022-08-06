dd28973c62c931d52785bda73953c218158ab875
Salah <salahzeinu@gmail.com>
2022-08-06 11:01:18 -0700

    return sall parameter

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,28 +1,27 @@
 #include "variadic_functions.h"
-
+#include <stdarg.h>
+#include <stdio.h>
 /**
- * sum_them_all - returns the sum of all its parameters.
- * @n: amount of the arguments summed
- *
- * Return: sum of its parameters.
+ * sum_them_all - Entry Point
+ * @n: variadic inputs
+ * Return: sum
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list arg;
-	unsigned int i;
-	unsigned int sum = 0;
+		va_list valist;
+			unsigned int i, sum = 0;
 
-	if (n == 0)
-		return (0);
+				if (n == 0)
+					return (0);
 
-	va_start(arg, n);
+				va_start(valist, n);
 
-	for (i = 0; i < n; i++)
-	{
-		sum += va_arg(arg, int);
-	}
+				for (i = 0; i < n; i++)
+				{
+				sum += va_arg(valist, const unsigned int);
+				}
 
-	va_end(arg);
+				va_end(valist);
 
-	return (sum);
+				return (sum);
 }
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,6 @@
+This is project is all about variadic functions and have the following functions.
+'1-a function that returns the sum of all its parameters.'
+'2-function that prints numbers, followed by a new line.'
+'3- a function that prints strings, followed by a new line.'
+'4 - a function that prints anything.'
+
