5572d6772b7f02b506b343ee73c04364a7dc1903
Salah <salahzeinu@gmail.com>
2022-08-28 03:44:39 -0700

    read-textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,38 +1,36 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
+
 /**
- * read_textfile - reads a text file and prints to  POSIX standard output.
- * @filename: a file
- * @letters: letters read and print
- * Return: number of letters it could read and print
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, nread, nwrite;
-	char *buff;
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
-	buff = malloc(letters * sizeof(char) + 1);
-	if (!buff)
-	i	return (0);
+
 	fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
-	{
-		free(buff);
 		return (0);
-	}
-	nread = read(fd, s, letters);
-	if (nread == -1)
-	{
-		free(buff);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
 		return (0);
-	}
-	nwrite = write(STDOUT_FILENO, s, nread);
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
 	close(fd);
-	free(buff);
-	if (nread == nwrite)
-		return (nwrite);
-	return (0);
+
+	free(buf);
+
+	return (nwr);
 }
