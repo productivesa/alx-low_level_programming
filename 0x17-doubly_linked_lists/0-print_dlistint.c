73c5e34ca927715478b201e5adab0ff9e9cec08c
productivesa <salahzeinu@gmail.com>
2022-09-29 13:51:43 -0700

    0-print_dlistint.c

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_dlistint - prints allall element
+ * @h: the head of the dlist
+ * Return: he number of nodes in the list
+ */
+size_t print_distint(const distint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		print("%d\n", h->n);
+		h = h->next;
+	}
+	return (nodes)
+}
