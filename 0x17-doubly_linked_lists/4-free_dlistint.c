5679ed4b69fe260ec660f9386621652a26703f2d
productivesa <salahzeinu@gmail.com>
2022-10-01 05:05:12 -0700

    4-free_dlistint.c

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,10 +1,9 @@
 #include "lists.h"
-
 /**
- *  * free_dlistint - frees a dlistint_t list
- *   * @head: head of the list
- *    * Return: no return
- *     */
+ * free_dlistint - frees a dlistint_t list
+ * @head: head of the list
+ * Return: no return
+ */
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *tmp;
