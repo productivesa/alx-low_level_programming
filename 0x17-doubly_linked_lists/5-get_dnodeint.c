f28f6dce6bb3369c688e5c05e29aabf2dd8e9c60
productivesa <salahzeinu@gmail.com>
2022-10-01 04:47:57 -0700

    5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * get_dnodeint_at_index - returns the
  * nth node of a dlistint_t linked list
@@ -9,7 +8,7 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	unsigned int i;
+	unsigned int x;
 
 	if (head == NULL)
 		return (NULL);
