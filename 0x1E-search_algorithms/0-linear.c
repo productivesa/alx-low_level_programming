94b51109091e838d306bb540e7162b8a0ff93b3f
productivesa <salahzeinu@gmail.com>
2023-02-12 03:37:38 -0800

    0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * linear_search - a function that searches for a value in an array 
+ * linear_search - a function that searches for a value in an array
  * of integers using the Linear search algorithm
  *
  * @array: input array
